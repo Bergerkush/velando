1828bb2c152b2de3db63bf2883fd9732be53fa78
Bergerkush <bergersubodh@gmail.com>
2026-04-28 15:44:36 +0200

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import '../styles/globals.css'
 import { CartProvider } from '../lib/cart'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
   return (
     <CartProvider>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-BECPWQMY4D"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BECPWQMY4D');
+        `}
+      </Script>
       <Component {...pageProps} />
     </CartProvider>
   )
