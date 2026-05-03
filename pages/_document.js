b4988b047f2637ac44d9a48a0a48b43d876a7234
Bergerkush <bergersubodh@gmail.com>
2026-05-03 14:10:11 +0200

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="de">
       <Head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="theme-color" content="#0f172a" />
@@ -13,6 +12,21 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+              (function(){
+                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+                s1.async=true;
+                s1.src='https://embed.tawk.to/69f736c60f7c9c1c2fb084c1/1jnmqrut9';
+                s1.charset='UTF-8';
+                s1.setAttribute('crossorigin','*');
+                s0.parentNode.insertBefore(s1,s0);
+              })();
+            `
+          }}
+        />
       </body>
     </Html>
   )
